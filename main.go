9276b70f0708d0a692d161221f32e6caaf6bb112
Ansaf Ahmad <ansafahmad70@gmail.com>
2017-02-18 21:20:56 -0500

    qrify dep

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	render "gopkg.in/unrolled/render.v1"
+
+	"log"
+
+	"github.com/gorilla/mux"
+	qrcode "github.com/skip2/go-qrcode"
+)
+
+func main() {
+
+	m := mux.NewRouter()
+	m.HandleFunc("/", IndexHandler)
+	m.HandleFunc("/qrify", QRHandler)
+	http.Handle("/", m)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+	r := render.New()
+	r.HTML(w, http.StatusOK, "example", nil)
+}
+
+func QRHandler(w http.ResponseWriter, req *http.Request) {
+	url := req.URL.Query().Get("url")
+	var png []byte
+	png, err := qrcode.Encode(url, qrcode.Medium, 256)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(png)
+
+}
